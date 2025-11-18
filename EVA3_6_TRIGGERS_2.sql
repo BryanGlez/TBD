c54db91653794eb0f455c800d8edd29ed8b4607d
BryanGlez <104457957+BryanGlez@users.noreply.github.com>
2025-11-18 15:16:41 -0600

    Add files via upload

---
+++ EVA3_6_TRIGGERS_2.sql
@@ -0,0 +1,6 @@
+DELIMITER $$
+CREATE DEFINER=`root`@`localhost` TRIGGER `personas_AFTER_INSERT` AFTER INSERT ON `personas` FOR EACH ROW 
+BEGIN
+    set @mensaje = "Persona agregada con exito";
+END$$
+DELIMITER ;
---
+++ EVA3_6_TRIGGERS_3.sql
@@ -0,0 +1,6 @@
+DELIMITER $$
+CREATE DEFINER=`root`@`localhost` TRIGGER `personas_AFTER_DELETE` AFTER DELETE ON `personas` FOR EACH ROW 
+BEGIN
+	insert into bitacora(descripcion) value (concat("Borrado", OLD.rfc));
+END$$
+DELIMITER ;
